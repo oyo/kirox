17d1b2e58e4be948406b1a849baa0a26e9d6d38e
Martin Rohrmeier <roryphant@gmail.com>
2026-06-02 09:06:41 +0200

    chore(tiles): cleanup

--- src/pages/Tiles/model.ts
+++ src/pages/Tiles/model.ts
@@ -86,35 +86,6 @@ export class TilesModel implements HexBoardModel {
     }
   }
 
-  /*
-  paintPath(item: BoardTile) {
-    const g = this.grid
-    const d = item.design
-    const s = item.state
-    const c = s.pathColor!
-    const p = s.pos
-    const odd = p.y % 2
-    const o = [
-      [-1, odd],
-      [0, 1],
-      [1, odd],
-      [1, odd - 1],
-      [0, -1],
-      [-1, odd - 1],
-    ]
-    const neighbors = o.map((_, i) => g[p.y + o[(s.rot + i) % 6][0]][p.x + o[(s.rot + i) % 6][1]])
-    for (let pi = 0; pi < d.paths.length; pi++) {
-      d.paths[pi]
-        .map((p) => neighbors[p])
-        .map(
-          (n, i) =>
-            (n.state.pathColor[n.design.pathIndex[(d.paths[pi][i] + s.rot + 9 - n.state.rot) % 6]] =
-              c[pi]),
-        )
-    }
-  }
-  */
-
   tap(pos: Coord) {
     const item = this.grid[pos.y][pos.x]
     this.grid.forEach((row) => row.forEach((tile) => (tile.state.pathColor = [0, 0, 0])))
